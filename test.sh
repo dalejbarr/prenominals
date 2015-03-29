0d122e0f255bdc29a77cfb0434c36d3b5b7530c9
dalejbarr <dalejbarr3@gmail.com>
2015-03-29 23:26:30 +0100

    bash scripts

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+
+xrandr --output DVI-I-1 --mode 1024x768 --output DP-0 --off
+sleep 2
+
+exp-eyelink/bin/expeyelink -expdb LexCompRI3_test.db -dbdriver sqlite -session $1 -width 1024 -height 768
+
+xrandr --output DVI-I-1 --auto --output DP-0 --auto --right-of DVI-I-1
---
+++ test_no_eyetracker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+
+exp-eyelink/bin/expdebug2 -expdb LexCompRI3_test.db -dbdriver sqlite -session $1 
+
